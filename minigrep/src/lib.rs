bf60d4abe23828d2cfc4ecea4be6d49e64ad1fdf
Peter Fomin <petenmail@rambler.ru>
2020-05-11 18:38:07 +0300

    minigrep initial commit

---
+++ minigrep/src/lib.rs
@@ -0,0 +1,70 @@
+use std::{env, fs, error::Error};
+
+pub struct Config {
+    pub query: String,
+    pub filename: String,
+    pub case_sensitive: bool,
+}
+
+impl Config {
+    pub fn new(args: &mut env::Args, ) -> Result<Self, &str> {
+        Ok(Self {
+            query: args.skip(1).next().ok_or_else(|| "No query input")?,
+            filename: args.next().ok_or_else(|| "No filename input")?,
+            case_sensitive: env::var("CASE_INSENSITIVE").is_err(),
+        })
+    }
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.filename)?;
+    let results = if config.case_sensitive {
+        search(&config.query, &contents)
+
+    } else {
+        search_case_insensitive(&config.query, &contents)
+    };
+    for line in results {
+        println!("{}", line);
+    }
+    Ok(())
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    contents.lines().filter(|line| line.contains(query)).collect()
+}
+
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    contents.lines().filter(|line| line.to_lowercase().contains(&query.to_lowercase())).collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn case_sensitive() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
+        );
+    }
+}
