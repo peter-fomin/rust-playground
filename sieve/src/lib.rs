48fe9751fd3b34e66fcc7b96817f647f6e888592
Peter Fomin <petenmail@rambler.ru>
2020-05-12 15:46:50 +0300

    Added solution for sieve of eratosphene problem

---
+++ sieve/src/lib.rs
@@ -0,0 +1,16 @@
+pub fn primes_up_to(upper_bound: usize) -> Vec<usize> {
+    // let upper_bound = upper_bound as usize;
+    let mut sieve: Vec<bool> = vec![true; upper_bound];
+    let mut primes: Vec<usize> = Vec::new();
+    for i in 2..=upper_bound {
+        if sieve[i - 2] {
+            primes.push(i);
+            let mut j = 2 * i;
+            while j <= upper_bound {
+                sieve[j - 2] = false;
+                j += i;
+            }
+        }
+    }
+    primes
+}
