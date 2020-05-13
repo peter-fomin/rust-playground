3f5212f213b4cfa1ffd1686223d5c12849ae9c92
Peter Fomin <petenmail@rambler.ru>
2020-05-13 14:07:56 +0300

    Introduced array.get(index)? instead of array.is_empty() check

--- binary-search/src/lib.rs
+++ binary-search/src/lib.rs
@@ -1,13 +1,9 @@
 use std::cmp::Ordering;
 
 pub fn find(array: &[i32], key: i32) -> Option<usize> {
-    if array.is_empty() {
-        return None;
-    }
-
     let index = array.len() / 2;
 
-    match array[index].cmp(&key) {
+    match array.get(index)?.cmp(&key) {
         Ordering::Equal => Some(index),
         Ordering::Greater => find(&array[..index], key),
         Ordering::Less => find(&array[(index + 1)..], key).map(|i| i + index + 1),
