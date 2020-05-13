3f0ae0c4e222b830977d5a7090c404d4da61fad3
Peter Fomin <petenmail@rambler.ru>
2020-05-13 22:14:15 +0300

    Implemented binary search for generics

--- binary-search/src/lib.rs
+++ binary-search/src/lib.rs
@@ -1,9 +1,14 @@
 use std::cmp::Ordering;
 
-pub fn find(array: &[i32], key: i32) -> Option<usize> {
+pub fn find<T: AsRef<[R]>, R: PartialOrd>(array: T, key: R) -> Option<usize> {
+    let array = array.as_ref();
     let index = array.len() / 2;
 
-    match array.get(index)?.cmp(&key) {
+    match array
+        .get(index)?
+        .partial_cmp(&key)
+        .unwrap_or(Ordering::Equal)
+    {
         Ordering::Equal => Some(index),
         Ordering::Greater => find(&array[..index], key),
         Ordering::Less => find(&array[(index + 1)..], key).map(|i| i + index + 1),
