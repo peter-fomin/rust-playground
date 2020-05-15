0b7f8d64f761e23593007db06a8748e6bc9435d1
Peter Fomin <petenmail@rambler.ru>
2020-05-15 16:22:44 +0300

    Refactor impl Clock block
    Introduce const for minutes in hour. Get rid of wrap_minutes associated
    function and implement proper methods that will mutate the struct
    instance instead of creating a new one.

--- clock/src/lib.rs
+++ clock/src/lib.rs
@@ -1,5 +1,7 @@
 use std::fmt;
-const MINUTES_IN_DAY: i32 = 1440;
+
+const MINUTES_IN_HOUR: i32 = 60;
+const MINUTES_IN_DAY: i32 = MINUTES_IN_HOUR * 24;
 
 #[derive(Debug, PartialEq, Eq)]
 pub struct Clock {
@@ -7,29 +9,23 @@ pub struct Clock {
 }
 
 impl Clock {
-    fn wrap_minutes(mut minutes: i32) -> i32 {
-        minutes %= MINUTES_IN_DAY;
-        if minutes < 0 {
-            minutes += MINUTES_IN_DAY;
-        }
-        minutes
-    }
-
     pub fn new(hours: i32, minutes: i32) -> Self {
-        Self {
-            minutes: Clock::wrap_minutes(minutes + 60 * hours),
-        }
+        Self { minutes: 0 }.set_minutes(hours * MINUTES_IN_HOUR + minutes)
     }
 
-    pub fn add_minutes(&self, minutes: i32) -> Self {
-        Self {
-            minutes: Clock::wrap_minutes(self.minutes + minutes),
-        }
+    pub fn set_minutes(mut self, minutes: i32) -> Self {
+        self.minutes = (MINUTES_IN_DAY + minutes % MINUTES_IN_DAY) % MINUTES_IN_DAY;
+        self
+    }
+
+    pub fn add_minutes(self, minutes: i32) -> Self {
+        let minutes = self.minutes + minutes;
+        self.set_minutes(minutes)
     }
 }
 
 impl fmt::Display for Clock {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{:02}:{:02}", self.minutes / 60, self.minutes % 60)
+        write!(f, "{:02}:{:02}", self.minutes / MINUTES_IN_HOUR, self.minutes % MINUTES_IN_HOUR)
     }
 }
