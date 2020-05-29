1a40b926da726dda44a5d301e31872e0aa8831a2
Peter Fomin <petenmail@rambler.ru>
2020-05-29 12:18:26 +0300

    Refactor code

--- nth-prime/src/lib.rs
+++ nth-prime/src/lib.rs
@@ -1,15 +1,16 @@
 pub fn nth(n: u32) -> u32 {
     // This function uses eratosthene sieve to find nth prime number.
-    let n = n as usize;
-    let mut max_size = n + 1; // Size of eratosthene seieve, for nth prime number should not be more than n * (ln(n) + ln(ln(n)))
+    let n = (n + 1) as usize; // In the task count starts from 0
     let mut primes: Vec<u32> = Vec::with_capacity(n);
-    if n > 1 {
-        let max_size_f64 = (n + 1) as f64;
-        max_size = (max_size_f64 * (max_size_f64.ln() + max_size_f64.ln().ln())) as usize;
-    }
+    let max_size = if n <= 2 { // Size of eratosthene seieve, for nth prime number should not be more than n * (ln(n) + ln(ln(n)))
+        n
+    } else {
+        let tmp = n as f64;
+        (tmp * (tmp.ln() + tmp.ln().ln())) as usize
+    };
     let mut eratosthene = vec![true; max_size];
     let mut i: usize = 2;
-    while primes.len() <= n {
+    while primes.len() < n {
         if eratosthene[i - 2] {
             primes.push(i as u32);
             let mut j = 2 * i;
