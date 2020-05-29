e814baa45d4f3a22588ed147af4a716d22cdc5c1
Peter Fomin <petenmail@rambler.ru>
2020-05-29 11:55:35 +0300

    Remove commented code

--- sieve/src/lib.rs
+++ sieve/src/lib.rs
@@ -1,5 +1,4 @@
 pub fn primes_up_to(upper_bound: usize) -> Vec<usize> {
-    // let upper_bound = upper_bound as usize;
     let mut sieve: Vec<bool> = vec![true; upper_bound];
     let mut primes: Vec<usize> = Vec::new();
     for i in 2..=upper_bound {
