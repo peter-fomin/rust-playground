22917a9251db93539178b9237dccb3b03aee8647
Peter Fomin <petenmail@rambler.ru>
2020-05-29 12:23:01 +0300

    Rename variable

--- nth-prime/src/lib.rs
+++ nth-prime/src/lib.rs
@@ -8,14 +8,14 @@ pub fn nth(n: u32) -> u32 {
         let tmp = n as f64;
         (tmp * (tmp.ln() + tmp.ln().ln())) as usize
     };
-    let mut eratosthene = vec![true; max_size];
+    let mut sieve = vec![true; max_size];
     let mut i: usize = 2;
     while primes.len() < n {
-        if eratosthene[i - 2] {
+        if sieve[i - 2] {
             primes.push(i as u32);
             let mut j = 2 * i;
             while j < max_size {
-                eratosthene[j - 2] = false;
+                sieve[j - 2] = false;
                 j += i;
             }
         }
