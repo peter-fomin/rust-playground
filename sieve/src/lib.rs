2411ada2468de7279a545baa2f1ad3474e25ee84
Peter Fomin <petenmail@rambler.ru>
2020-05-30 16:24:45 +0300

    Introduce a for cycle

--- sieve/src/lib.rs
+++ sieve/src/lib.rs
@@ -1,13 +1,11 @@
 pub fn primes_up_to(upper_bound: usize) -> Vec<usize> {
-    let mut sieve: Vec<bool> = vec![true; upper_bound];
+    let mut sieve: Vec<bool> = vec![true; upper_bound + 1];
     let mut primes: Vec<usize> = Vec::new();
     for i in 2..=upper_bound {
-        if sieve[i - 2] {
+        if sieve[i] {
             primes.push(i);
-            let mut j = 2 * i;
-            while j <= upper_bound {
-                sieve[j - 2] = false;
-                j += i;
+            for j in ((i * i)..=upper_bound).step_by(i) {
+                sieve[j] = false;
             }
         }
     }
